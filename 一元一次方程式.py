c4fb1c8157041b5ec66363892bb658accc9c1afa
JavaOliverLu <70323169+JavaOliverLu@users.noreply.github.com>
2021-02-04 09:48:07 +0800

    一元一次方程式.py
    math

---
+++ 一元一次方程式.py
@@ -0,0 +1,10 @@
+print('ax+b=cx+d')
+a=int(input('please enter a'))
+b=int(input('please enter b'))
+c=int(input('please enter c'))
+d=int(input('please enter d'))
+e=c-a
+f=b-d
+ans=e/f
+print('the root of x is')
+print(ans)
